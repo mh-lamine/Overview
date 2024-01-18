d100e0ae3c606ca1fa89fdb2b9aa3f1761e12ab7
Mohamed-Lamine Ngom <lamzo@MacBook-Pro-de-Mohamed-Lamine.local>
2024-01-18 16:28:41 +0100

    various changes, overall improvment

--- backend/controllers/appointmentsController.js
+++ backend/controllers/appointmentsController.js
@@ -1,13 +1,31 @@
-const getAppointments = (req, res) => {
-  res.status(200).send("get appointments");
-  console.log(req.body);
+const Appointments = require("../models/appointmentModel");
+
+const getAppointments = async (req, res) => {
+  const appointments = await Appointments.find();
+  res.status(200).json(appointments);
 };
 
-const setAppointment = (req, res) => {
-  res.status(200).send("set appointment");
+const setAppointment = async (req, res) => {
+  if (!req.body.name || !req.body.description) {
+    res.status(400).send("Missing name or description");
+    return;
+  }
+  const appointment = await Appointments.create({
+    name: req.body.name,
+    description: req.body.description,
+  });
+  res.status(201).json(appointment);
 };
 
-const updateAppointment = (req, res) => {
+const updateAppointment = async (req, res) => {
+  const appointment = await Appointments.findById(req.params.id);
+  if (!appointment) {
+    res.status(404).send("Appointment not found");
+    return;
+  }
+  const updatedAppointment = await Appointments.findByIdAndUpdate(
+    req.params.id, req.body, { new: true }
+  );
   res.status(200).send(`update appointment ${req.params.id}`);
 };
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const connectDB = require('./config/db');
+connectDB();
+
 app.use('/appointments', require('./routes/appointmentsRoutes'));
 
 app.listen(3000, () => console.log(`http://localhost:3000`));
